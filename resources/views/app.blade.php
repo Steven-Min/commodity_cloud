94f54561d7094698736a9ef3a23c0e6bd3917657
Steven-Min <itcloud02147@gmail.com>
2023-08-31 17:45:26 +0800

    line awesomeアイコン追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" value="{{ csrf_token() }}"/>
         <title>{{env('APP_NAME')}}</title>
+        <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
     </head>
     <body>
